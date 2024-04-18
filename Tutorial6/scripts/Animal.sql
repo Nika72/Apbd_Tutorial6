66a91a8d8e2540c48633d22d6475a6d9546404dc
s25686 <s25686@pjwstk.edu.pl>
2024-04-18 23:48:29 +0200

    change

--- Tutorial6/scripts/Animal.sql
+++ Tutorial6/scripts/Animal.sql
@@ -1,8 +1,17 @@
+-- Create a new table with the identity column
 CREATE TABLE Animal (
-    IdAnimal INT PRIMARY KEY,
+    IdAnimal INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(200),
     Description NVARCHAR(200),
     Category NVARCHAR(200),
     Area NVARCHAR(200)
 );
-select * from Animal;
+
+-- Copy data from old table to new table
+SET IDENTITY_INSERT Animal ON;
+INSERT INTO Animal (IdAnimal, Name, Description, Category, Area)
+SELECT IdAnimal, Name, Description, Category, Area
+FROM Animal;
+
+SET IDENTITY_INSERT Animal OFF;
+
